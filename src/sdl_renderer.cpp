46996d882c78d5b52555e122bed827793d13a231
Stephen Collins <stephenc211@gmail.com>
2018-05-27 22:20:13 -0400

    WIP drawing debug window

--- src/sdl_renderer.cpp
+++ src/sdl_renderer.cpp
@@ -1,12 +1,59 @@
 #include "./sdl_renderer.h"
 
+const char* DEBUG_FONT = "./fonts/OpenSans-Regular.ttf";
+
 void SDLRenderer::render() {
   // render stuff
   SDL_RenderClear(this->sdlRendererPtr);
 
   // this->renderSceneGraph(renderer,game);
+  // TODO: for now, just draw the debug window, ultimately (maybe?) wrapping this probably in
+  // preprocessor directives
+  this->renderDebugWindow();
 
   SDL_RenderPresent(this->sdlRendererPtr);
+}
+
+// you have to load a font file to then use
+TTF_Font* SDLRenderer::loadFont(const char *fileName, int fontSize) {
+  TTF_Font* font = TTF_OpenFont(fileName, fontSize); //this opens a font style and sets a size
+  // returns a pointer to a font
+  return font;
+}
+
+void SDLRenderer::renderDebugWindowTextLine(const char* debugText) {
+  SDL_Color White = {255, 255, 255};  // this is the color in rgb format, maxing out all would give you the color white, and it will be your text's color
+
+  SDL_Surface* surfaceMessage = TTF_RenderText_Solid(this->debugFont, debugText, White); // as TTF_RenderText_Solid could only be used on SDL_Surface then you have to create the surface first
+
+  SDL_Texture* Message = SDL_CreateTextureFromSurface(this->sdlRendererPtr, surfaceMessage); //now you can convert it into a texture
+  SDL_FreeSurface(surfaceMessage);
+
+
+  SDL_Rect Message_rect; //create a rect
+  Message_rect.x = this->debugWindowRect.x;  //controls the rect's x coordinate 
+  Message_rect.y = this->debugWindowRect.y; // controls the rect's y coordinte
+  Message_rect.w = 150; // controls the width of the rect
+  Message_rect.h = 25; // controls the height of the rect
+
+  //Mind you that (0,0) is on the top left of the window/screen, think a rect as the text's box, that way it would be very simple to understance
+
+  //Now since it's a texture, you have to put RenderCopy in your game loop area, the area where the whole code executes
+
+  SDL_RenderCopy(this->sdlRendererPtr, Message, NULL, &Message_rect); //you put the renderer's name first, the Message, the crop size(you can ignore this if you don't want to dabble with cropping), and the rect which is the size and coordinate of your texture
+  SDL_DestroyTexture(Message);
+}
+
+void SDLRenderer::renderDebugWindow() {
+  // draw debug window
+  
+  // first, a black rectangle - for now, completely opaque
+  SDL_SetRenderDrawColor(this->sdlRendererPtr, 0, 0, 0, 255);
+  SDL_RenderFillRect(this->sdlRendererPtr, &this->debugWindowRect);
+  // next draw text
+  this->renderDebugWindowTextLine("Dummy Debug Text");
+
+  SDL_SetRenderDrawColor(this->sdlRendererPtr, 178, 232, 255, 255);
 }
 
 double SDLRenderer::getTime() {
@@ -26,11 +73,16 @@ int SDLRenderer::init(int SCREEN_WIDTH, int SCREEN_HEIGHT) {
     printf("TTF_Init: %s\n", TTF_GetError());
     return 1;
   }
+  this->debugWindowRect.x = 20;
+  this->debugWindowRect.y = 20;
+  this->debugWindowRect.w = 180;
+  this->debugWindowRect.h = 150;
+
+  this->debugFont = this->loadFont(DEBUG_FONT, 16);
   // TODO: maybe FIXME: for now, if no joystick (PS4 typically) is connected, assume just keyboard
   // maybe add a preprocessor directive here
   // Check for joysticks
-  if( SDL_NumJoysticks() < 1 )
-  {
+  if( SDL_NumJoysticks() < 1 ) {
     printf( "Warning: No joysticks connected!\n" );
     this->sdlJoystick = nullptr;
   }
@@ -81,7 +133,7 @@ int SDLRenderer::init(int SCREEN_WIDTH, int SCREEN_HEIGHT) {
     printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
     return 1;
   }   
-  
+  // set initial background color
   SDL_SetRenderDrawColor(this->sdlRendererPtr, 178, 232, 255, 255);
   return 0;
 }
@@ -96,4 +148,9 @@ void SDLRenderer::cleanUp() {
   SDL_Quit();
 }
 
-SDLRenderer::~SDLRenderer() {};
+SDLRenderer::~SDLRenderer() {
+  if (this->debugFont) {
+    TTF_CloseFont(this->debugFont);
+    this->debugFont = nullptr;
+  }
+};
--- src/sdl_renderer.h
+++ src/sdl_renderer.h
@@ -8,10 +8,15 @@ class SDLRenderer: public Renderer {
     SDL_Renderer* sdlRendererPtr;
     SDL_Window* sdlWindowPtr;
     SDL_Joystick* sdlJoystick;
+    SDL_Rect debugWindowRect;
+    TTF_Font* debugFont;
     void render();
     double getTime();
     int init(int SCREEN_WIDTH, int SCREEN_HEIGHT);
+    void renderDebugWindowTextLine(const char* debugText);
+    void renderDebugWindow();
     void cleanUp();
+    TTF_Font* loadFont(const char *fileName, int fontSize);
     ~SDLRenderer();
     // void renderSceneGraph();
 };
